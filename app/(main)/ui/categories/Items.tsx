0ef4820e3e0e93b897fe7e4bc81caeafd95321c6
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-09-02 17:49:27 +0300

    edit category item

--- app/(main)/ui/categories/Items.tsx
+++ app/(main)/ui/categories/Items.tsx
@@ -5,6 +5,7 @@ import cat2 from "@/public/assets/categories/cat2.webp";
 import cat3 from "@/public/assets/categories/cat3.webp";
 import cat4 from "@/public/assets/categories/cat4.jpeg";
 import Image from "next/image";
+import Link from "next/link";
 
 const categorieItems = [
   {
@@ -33,28 +34,35 @@ const Items = () => {
   return (
     <>
       {categorieItems.map((item, index) => (
-        <motion.div
-          key={item.id}
-          className="relative overflow-hidden rounded-lg"
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ delay: index * 0.2, duration: 0.3 }}
-          whileHover={{ scale: 1.05 }} // Add hover animation
-        >
-          <Image
-            src={item.image}
-            alt={item.name}
-            width={300}
-            height={300}
-            className="w-full h-full object-cover"
-          />
+        <Link href={`/categories/${item.name}`} key={item.id}>
           <motion.div
-            className="absolute top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center text-white text-2xl font-bold"
-            whileHover={{ opacity: 0 }} // Hide text on hover
+            className="relative overflow-hidden rounded-lg transition-all duration-100 cursor-pointer"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.2, duration: 0.3 }}
+            whileHover={{ scale: 1.02 }} // Add hover animation
           >
-            {item.name}
+            <div className="relative">
+              <Image
+                src={item.image}
+                alt={item.name}
+                width={300}
+                height={200}
+                className="w-full h-[200px] object-cover"
+              />
+            </div>
+            <motion.div
+              className="hidden absolute top-0 left-0 w-full h-full bg-black/50 sm:flex items-center justify-center text-white text-2xl font-bold"
+              whileHover={{ opacity: 0 }} // Hide text on hover
+            >
+              {item.name}
+            </motion.div>
+            {/* Add text for small screens */}
+            <div className="sm:hidden">
+              <h3 className="text-center text-lg font-semibold">{item.name}</h3>
+            </div>
           </motion.div>
-        </motion.div>
+        </Link>
       ))}
     </>
   );
