6995192ccb9bc8083b2be4f183e2133f6f334133
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-09-18 16:02:01 +0300

    last update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 import StoreProvider from "@/redux/StoreProvider";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ScrollToTop from "@/components/ScrollToTop";
 // import fonts
 import { mainFont } from "./fonts/fonts";
 import "./globals.css";
+
+const ToastContainer = dynamic(
+  () => import("react-toastify").then((mod) => mod.ToastContainer),
+  { ssr: false },
+);
+const ScrollToTop = dynamic(() => import("@/components/ScrollToTop"), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
@@ -16,8 +23,10 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         <StoreProvider>
           {children}
-          <ToastContainer position="top-right" autoClose={5000} />
-          <ScrollToTop />
+          <Suspense fallback={null}>
+            <ToastContainer position="top-right" autoClose={5000} />
+            <ScrollToTop />
+          </Suspense>
         </StoreProvider>
       </body>
     </html>
