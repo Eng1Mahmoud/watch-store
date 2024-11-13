5f5880f95ac8fa4e32e6c0892b34cdf48da63a07
Mahmoud Mohamed <eslam.mohamed@fci.sohag.edu.eg>
2024-11-13 15:24:00 +0200

    send lang in reqest header

--- axios/axiosClientInstance.ts
+++ axios/axiosClientInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getTokenClient } from "@/utils/getTokenClient";
+import { getCookie } from "cookies-next";
 import {
   baseConfig,
   setContentType,
@@ -13,6 +14,11 @@ axiosClientInstance.interceptors.request.use((config) => {
   const token = getTokenClient();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+  }
+  // Get locale from cookies and set it to the headers
+  const locale = getCookie("NEXT_LOCALE");
+  if (locale) {
+    config.headers["Accept-Language"] = locale;
   }
   return setContentType(config);
 });
